15b86def0833592ce5e805b9ffb864700baebd24
Birim Sain <birims@gmail.com>
2015-11-06 14:49:09 +0200

    dosya ismi değişkeni düzeldi

--- altyazibir.sh
+++ altyazibir.sh
@@ -1,8 +1,8 @@
 echo $1
-cikis="/home/birim"
-dosya="${1%.*}"
+cikis="/home/birim/"
+dosya="${1##*/}"
 deneme=${1%%mkv}
 srt="$deneme""srt"
 #iconv -t UTF-8 -f ISO-8859-9 "$srt" > "${srt}s"
 #mv "${srt}s" "${srt%.srts}"
-sudo mkvmerge -o "$cikis""$dosya"".mkv" -S "$1" "$srt"
+sudo mkvmerge -o "$cikis""$dosya" -S "$1" "$srt"
