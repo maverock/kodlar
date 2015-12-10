24aaf860f22349801de1ddc99d2649dc1617570c
Debian <admin@ip-172-31-7-181.eu-central-1.compute.internal>
2015-12-10 10:01:16 +0200

    morethanneed eklendi

--- awscontrol.py
+++ awscontrol.py
@@ -10,11 +10,11 @@ Supervisor_Count=0
 System_Under_Stres=0
 Last_Supervisor_Add=0
 Wait_Until=0
+More_Than_Need=0
 
 def LoadAverage():
     Deger=""
     Toplam=0
-    #api_url = "https://graphite.8digits.com/render?width=400&from=-4minutes&until=now&height=250&hideLegend=true&tz=Europe%2FIstanbul&target=servers.ip-10-0-0-37.loadavg.01&target=servers.ip-10-0-0-29.loadavg.01&target=servers.ip-10-0-0-73.loadavg.01&target=servers.ip-10-0-0-36.loadavg.01&target=servers.ip-10-0-0-38.loadavg.01&target=servers.ip-10-0-0-34.loadavg.01&target=servers.ip-10-0-0-35.loadavg.01&title=Supervisor%20Load&_uniq=0.34630449186079204&format=json"
     api_url= Url_Builder()
     r = requests.get(api_url)
     data = json.loads(r.text)
@@ -26,8 +26,6 @@ def LoadAverage():
 		Toplam=Toplam+Deger
 		break
 	#print x['target'],Deger
-    
-    #print "Ortalama=",Toplam/LineCount
     return Toplam/LineCount
 
 def Url_Builder():
@@ -54,35 +52,44 @@ def Log( s ):
 def ReadConfig():
     with open('/home/admin/birim_kodlar/runtime.conf', 'r') as file:
 	configdata = file.readlines()
-    global Supervisor_Count,System_Under_Stres,Last_Supervisor_Add,Wait_Until
-    Supervisor_Count=configdata[0].split("=")[1]
-    System_Under_Stres=configdata[1].split("=")[1]
-    Last_Supervisor_Add=configdata[2].split("=")[1]
-    Wait_Until=configdata[3].split("=")[1]
+    global Supervisor_Count,System_Under_Stres,Last_Supervisor_Add,Wait_Until,More_Than_Need
+    Supervisor_Count=configdata[0].split("=")[1].strip()
+    System_Under_Stres=configdata[1].split("=")[1].strip()
+    Last_Supervisor_Add=configdata[2].split("=")[1].strip()
+    Wait_Until=configdata[3].split("=")[1].strip()
+    More_Than_Need=configdata[4].split("=")[1].strip()
 
 def WriteConfig():
-    global Supervisor_Count,System_Under_Stres,Last_Supervisor_Add,Wait_Until
+    global Supervisor_Count,System_Under_Stres,Last_Supervisor_Add,Wait_Until,More_Than_Need
     with open('/home/admin/birim_kodlar/runtime.conf', 'w') as file:
-        file.write("Supervisor_Count="+Supervisor_Count )
+        file.write("Supervisor_Count="+str(Supervisor_Count)+"\n" )
 	file.write("System_Under_Stres="+str(System_Under_Stres)+"\n")
-	file.write("Last_Supervisor_Add="+str(Last_Supervisor_Add))
-	file.write("Wait_Until="+Wait_Until)
+	file.write("Last_Supervisor_Add="+str(Last_Supervisor_Add)+"\n")
+	file.write("Wait_Until="+str(Wait_Until)+"\n")
+	file.write("More_Than_Need="+str(More_Than_Need)+"\n")
 	file.close()
 
 def Control():
+    global Supervisor_Count,System_Under_Stres,Last_Supervisor_Add,Wait_Until,More_Than_Need
     if Wait_Until>datetime.datetime.now().strftime("%Y-%m-%d %H:%M"):
 	Log("Bekleme suresi etkin")
 	return
-    if LoadAverage() > 5 :
-	global System_Under_Stres,Last_Supervisor_Add
-	Last_Supervisor_Add=int(Last_Supervisor_Add)+1
+#########Sistem stres altinda
+    if LoadAverage() > 10 :
 	System_Under_Stres=int(System_Under_Stres)+1
 	Log("Mevcut Yuk Sistem Icin Fazla= "+ str(LoadAverage()))
 	WriteConfig()
+#########Supervisor sayisi fazla
     if LoadAverage() < 5 :
-	Log("Supervisor sayisi suan icin fazla ortalama yuk = "+str(LoadAverage()))
+	More_Than_Need=int(More_Than_Need)+1
+	Log("Supervisor sayisi suan icin fazla ortalama yuk = "+str(LoadAverage())+"	gerceklesme sayisi "+str(More_Than_Need))
+	WriteConfig()
+#########Sistem sorunsuz calisiyor
     if LoadAverage() > 5 and LoadAverage() < 10 :
-	Log("Suan sistem sorunsuz calismakta= "+ str(LoadAverage()))
+	System_Under_Stres=0
+	More_Than_Need=0
+	Log("Suan sistem sorunsuz calismakta= "+ str(LoadAverage())+"	tum sayaclar sifirlandi")
+	WriteConfig()
 
 
 #Gonder="Mevcut yuk="+str(LoadAverage())
